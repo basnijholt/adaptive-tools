dac44db58d8e3235822d1ce6e2a0b6ccc66f4c07
Bas Nijholt <basnijholt@gmail.com>
2018-06-05 15:38:41 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.6
+# -*-Python-*-
+
+from setuptools import setup
+
+
+setup(name='adaptive_tools',
+      version='0.2.1',
+      description='Tools that extend the use of https://github.com/python-adaptive/adaptive.',
+      url='https://github.com/basnijholt/adaptive_tools',
+      author='Bas Nijholt',
+      license='BSD 3-clause',
+      py_modules=["adaptive_tools"],
+)
