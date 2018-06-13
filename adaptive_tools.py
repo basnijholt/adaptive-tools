3bf6036d8c3a366240a956a5c04e312d1cf8d538
Bas Nijholt <basnijholt@gmail.com>
2018-06-13 11:40:13 -0700

    add AverageLearner and simplify code

--- adaptive_tools.py
+++ adaptive_tools.py
@@ -16,41 +16,40 @@ import adaptive
 import toolz
 
 
+def save(fname, data, compress=True):
+    os.makedirs(os.path.dirname(fname), exist_ok=True)
+    _open = gzip.open if compress else open
+    with _open(fname, 'wb') as f:
+        pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def load(self, fname, compress=True):
+    _open = gzip.open if compress else open
+    with _open(fname, 'rb') as f:
+        return pickle.load(f)
+
+
 class Learner1D(adaptive.Learner1D):
 
-    def save(self, folder, fname, compress=True):
-        os.makedirs(folder, exist_ok=True)
-        fname = os.path.join(folder, fname)
-        _open = gzip.open if compress else open
-        with _open(fname, 'wb') as f:
-            pickle.dump(self.data, f, protocol=pickle.HIGHEST_PROTOCOL)
+    def save(self, fname, compress=True):
+        save(fname, self.data, compress)
 
-    def load(self, folder, fname, compress=True):
-        _open = gzip.open if compress else open
-        fname = os.path.join(folder, fname)
+    def load(self, fname, compress=True):
         try:
-            with _open(fname, 'rb') as f:
-                self.data = pickle.load(f)
+            self.data = load(self, fname, compress)
         except FileNotFoundError:
             pass
 
 
 class Learner2D(adaptive.Learner2D):
 
-    def save(self, folder, fname, compress=True):
-        os.makedirs(folder, exist_ok=True)
-        fname = os.path.join(folder, fname)
-        _open = gzip.open if compress else open
-        with _open(fname, 'wb') as f:
-            pickle.dump(self.data, f, protocol=pickle.HIGHEST_PROTOCOL)
+    def save(self, fname, compress=True):
+        save(fname, self.data, compress)
 
-    def load(self, folder, fname, compress=True):
-        _open = gzip.open if compress else open
-        fname = os.path.join(folder, fname)
+    def load(self, fname, compress=True):
         try:
-            with _open(fname, 'rb') as f:
-                self.data = pickle.load(f)
-                self.refresh_stack()
+            self.data = load(self, fname, compress)
+            self.refresh_stack()
         except FileNotFoundError:
             pass
 
@@ -61,32 +60,46 @@ class Learner2D(adaptive.Learner2D):
                 self._stack.pop(point)
 
 
+class AverageLearner(adaptive.AverageLearner):
+
+    def save(self, fname, compress=True):
+        data = (self.data, self.npoints, self.sum_f, self.sum_f_sq)
+        save(fname, data, compress)
+
+    def load(self, fname, compress=True):
+        try:
+            data = load(self, fname, compress)
+            self.data, self.npoints, self.sum_f, self.sum_f_sq = data
+        except FileNotFoundError:
+            pass
+
+
+def get_fname(i, fname_pattern=None):
+    fname_pattern = 'data_learner_{}.pickle' or fname_pattern
+    return fname_pattern.format(f'{i:05d}')
+
+
 class BalancingLearner(adaptive.BalancingLearner):
 
-    def save(self, folder, fname_pattern='data_learner_{}.pickle',
-             compress=True):
-        os.makedirs(folder, exist_ok=True)
+    def save(self, folder, fname_pattern=None, compress=True):
         for i, learner in enumerate(self.learners):
-            fname = fname_pattern.format(f'{i:04d}')
-            learner.save(folder, fname, compress=compress)
+            fname = get_fname(i, fname_pattern)
+            learner.save(os.path.join(folder, fname), compress=compress)
 
-    def load(self, folder, fname_pattern='data_learner_{}.pickle',
-             compress=True):
+    def load(self, folder, fname_pattern=None, compress=True):
         for i, learner in enumerate(self.learners):
-            fname = fname_pattern.format(f'{i:04d}')
-            learner.load(folder, fname, compress=compress)
+            fname = get_fname(i, fname_pattern)
+            learner.load(os.path.join(folder, fname), compress=compress)
 
-    async def _periodic_saver(self, runner, folder,
-                             fname_pattern, interval, compress):
+    async def _periodic_saver(self, runner, folder, fname_pattern, interval, compress):
         while runner.status() == 'running':
             await asyncio.sleep(interval)
             self.save(folder, fname_pattern, compress)
 
-    def start_periodic_saver(self, runner, folder,
-                             fname_pattern='data_learner_{}.pickle',
+    def start_periodic_saver(self, runner, folder, fname_pattern=None,
                              interval=3600, compress=True):
-        saving_coro = self._periodic_saver(runner, folder, fname_pattern,
-                                          interval, compress)
+        saving_coro = self._periodic_saver(runner, folder, fname_pattern, 
+                                           interval, compress)
         return runner.ioloop.create_task(saving_coro)
 
 
@@ -94,22 +107,26 @@ class BalancingLearner(adaptive.BalancingLearner):
 # Running multiple runners, each on its own core. #
 ###################################################
 
-def run_learner_in_ipyparallel_client(learner, goal, profile, folder,
+def _run_learner_in_ipyparallel_client(learner, goal, profile, folder,
                                       fname_pattern, periodic_save, timeout,
-                                      save_interval):
+                                      save_interval, client_kwargs):
     import hpc05
     import zmq
     import adaptive
     import asyncio
 
-    client = hpc05.Client(profile=profile, context=zmq.Context(), timeout=timeout)
+    client = hpc05.Client(profile=profile,
+                          context=zmq.Context(),
+                          timeout=timeout,
+                          hostname=hostname,
+                          **client_kwargs)
     client[:].use_cloudpickle()
     loop = asyncio.new_event_loop()
     runner = adaptive.Runner(learner, executor=client, goal=goal, ioloop=loop)
 
     if periodic_save:
         try:
-            save_task = learner.start_periodic_saver(runner, folder, fname_pattern,
+            save_task = learner.start_periodic_saver(runner, fname_pattern,
                                                      save_interval)
         except AttributeError:
             raise Exception(f'Cannot auto-save {type(learner)}.')
@@ -119,8 +136,9 @@ def run_learner_in_ipyparallel_client(learner, goal, profile, folder,
 
 
 def split_learners_in_executor(learners, executor, profile, ncores, goal=None,
-                               folder='tmp-{}', fname_pattern='data_learner_{}.pickle',
-                               periodic_save=True, timeout=300, save_interval=3600):
+                               folder='tmp-{}', fname_pattern=None,
+                               periodic_save=True, timeout=300, save_interval=3600,
+                               client_kwargs={}):
     if goal is None:
         if not periodic_save:
             raise Exception('Turn on periodic saving if there is no goal.')
@@ -129,10 +147,10 @@ def split_learners_in_executor(learners, executor, profile, ncores, goal=None,
     futs = []
     for i, _learners in enumerate(split(learners, ncores)):
         learner = BalancingLearner(_learners)
-        fut = executor.submit(run_learner_in_ipyparallel_client, learner,
-                              goal, profile, folder.format(f'{i:04d}'),
+        fut = executor.submit(_run_learner_in_ipyparallel_client, learner,
+                              goal, profile, folder.format(f'{i:05d}'),
                               fname_pattern, periodic_save, timeout,
-                              save_interval)
+                              save_interval, client_kwargs)
         futs.append(fut)
     return futs
 
