8b9d3d2d7b0154127e0ad12f4005403804884dba
Bas Nijholt <basnijholt@gmail.com>
2018-06-16 01:35:32 +0200

    only use cores with runner for the runners

--- adaptive_tools.py
+++ adaptive_tools.py
@@ -118,7 +118,7 @@ class Runner(adaptive.Runner):
 # Running multiple runners, each on its own core. #
 ###################################################
 
-def run_learner_in_ipyparallel_client(learner, goal, interval, save_kwargs, client_kwargs):
+def run_learner_in_ipyparallel_client(learner, goal, interval, save_kwargs, client_kwargs, ncores):
     import ipyparallel
     import zmq
     import adaptive
@@ -127,7 +127,7 @@ def run_learner_in_ipyparallel_client(learner, goal, interval, save_kwargs, clie
     client = ipyparallel.Client(context=zmq.Context(), **client_kwargs)
     client[:].use_cloudpickle()
     loop = asyncio.new_event_loop()
-    runner = Runner(learner, executor=client, goal=goal, ioloop=loop)
+    runner = Runner(learner, executor=client.executor(slice(ncores, None)), goal=goal, ioloop=loop)
     save_task = runner.start_periodic_saver(save_kwargs, interval)
     loop.run_until_complete(runner.task)
     return learner
@@ -137,7 +137,7 @@ default_client_kwargs = dict(profile='pbs', timeout=300, hostname='hpc05')
 default_save_kwargs = dict(folder='tmp-{}')
 
 
-def split_learners_in_executor(learners, executor, ncores, goal=None, interval=3600,
+def split_learners_in_executor(learners, client, ncores, goal=None, interval=3600,
                                save_kwargs=default_save_kwargs,
                                client_kwargs=default_client_kwargs):
     if goal is None:
@@ -148,8 +148,10 @@ def split_learners_in_executor(learners, executor, ncores, goal=None, interval=3
     futs = []
     for i, _learners in enumerate(split(learners, ncores)):
         learner = BalancingLearner(_learners)
-        fut = executor.submit(run_learner_in_ipyparallel_client, learner,
-                              goal, interval, save_kwargs, client_kwargs)
+        fut = client[i].executor.submit(
+            run_learner_in_ipyparallel_client, learner,
+            goal, interval, save_kwargs, client_kwargs, ncores
+        )
         futs.append(fut)
     return futs
 
